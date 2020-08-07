08f0fe10338ec105b8e551a2617951bb61473de1
PingPengPong <pingpengpongpog@gmail.com>
2020-08-07 14:03:15 -0700

    git commit

--- MonkyS.py
+++ MonkyS.py
@@ -41,43 +41,6 @@ async def on_message(message):
         if message.content.startswith('APPLY'):
             await message.author.send('What roles are you applying for?')
 
-@client.event
-async def on_message(message):
-    if message.guild is None:
-        await message.author.send('How many chapters can you do per week?')
-
-@client.event
-async def on_message(message):
-    if message.guild is None:
-        await message.author.send('Do you understand that this is volunteer work?')
-
-@client.event
-async def on_message(message):
-    if message.guild is None:
-        await message.author.send('Link your test OR past work here (if test, give us permission to edit/suggest)')
-
-@client.event
-async def on_message(message):
-    if message.guild is None:
-        await message.author.send("Lastly, please type 'DONE' to submit your application.")
-
-@client.event
-async def on_message(message):
-    if message.guild is None:
-        if message.content.startswith('DONE'):
-            channel = client.get_channel(738107566692761721)
-            NASA1 = client.get_user(315869723373862917)
-            NASA2 = client.get_user(208108164061593600)
-            NASA3 = client.get_user(472238811691352065)
-            NASA4 = client.get_user(564523794920767488)
-            await message.author.send('Thank you for your interest in joining Monky Scnas, we\'ll get back to you ASAP! :)')
-            await channel.send(f'New app has been submitted {NASA1}, {NASA2}, {NASA3}, {NASA4}')
-
-
-            def check(m):
-                return m.content == 'DONE' and m.channel == channel
-
-            msg = await client.wait_for('DONE', check=check)
 
 
 # Run command: important always keep at the end
