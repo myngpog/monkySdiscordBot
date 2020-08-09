6e8759b7f43b7e872b36256c8aed56c1398b16ee
PingPengPong <pingpengpongpog@gmail.com>
2020-08-08 19:11:37 -0700

    git commit

--- MonkyS.py
+++ MonkyS.py
@@ -58,7 +58,7 @@ async def on_message(message):
             # DONE or CANCEL NEEDS WORKING
             def check(d):
                 return d.content == 'DONE' and d.channel == message.channel
-            if True: await client.wait_for('message', check=check):
+            if await client.wait_for('message', check=check):
                 channel2 = client.get_channel(738107566692761721)
                 NASA1 = client.get_user(315869723373862917)
                 NASA2 = client.get_user(208108164061593600)
@@ -68,7 +68,7 @@ async def on_message(message):
                 await channel2.send(f'New Monky application pog! {NASA1}, {NASA2}, {NASA3}, {NASA4}')
             def check(c):
                 return c.content == 'CANCEL' and c.channel == message.channel
-            if else: await client.wait_for('message', check=check):
+            if await client.wait_for('message', check=check):
                 await message.author.send('Sorry to see you go D^: we hope you continue to support us!')
 
 
