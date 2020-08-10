e851d7765155317195a2d553348d2a4c4df6bf6c
PingPengPong <pingpengpongpog@gmail.com>
2020-08-09 20:48:42 -0700

    git commit

--- MonkyS.py
+++ MonkyS.py
@@ -55,20 +55,20 @@ async def on_message(message):
             # Finale
             await client.wait_for('message', check=check)
             if True: await message.author.send("Lastly, please type 'DONE' to submit your application, or 'CANCEL' to cancel.")
-            # if DONE
+            # final step
             def check(d):
                 return d.content == ('DONE' and d.channel == message.channel) or ('CANCEL' and d.channel == message.channel)
-            if await client.wait_for('message', check=check):
-                channel2 = client.get_channel(738107566692761721)
-                NASA1 = client.get_user(315869723373862917)
-                NASA2 = client.get_user(208108164061593600)
-                NASA3 = client.get_user(472238811691352065)
-                NASA4 = client.get_user(564523794920767488)
-                await message.author.send('Thank you for your application! We\'ll get back to you ASAP!')
-                await channel2.send(f'New Monky application pog! {NASA1}, {NASA2}, {NASA3}, {NASA4}')
-
-                await message.author.send('Sorry to see you go D^: we hope you continue to support us!')
-
+            await client.wait_for('message', check=check)
+            channel2 = client.get_channel(738107566692761721)
+            NASA1 = client.get_user(315869723373862917)
+            NASA2 = client.get_user(208108164061593600)
+            NASA3 = client.get_user(472238811691352065)
+            NASA4 = client.get_user(564523794920767488)
+            # if DONE
+            await message.author.send('Thank you for your application! We\'ll get back to you ASAP!')
+            await channel2.send(f'New Monky application, pog! {NASA1}, {NASA2}, {NASA3}, {NASA4}')
+            # if CANCEL
+            await message.author.send('Sorry to see you go D^: we hope you continue to support us!')
 
 
 # Run command: important always keep at the end
