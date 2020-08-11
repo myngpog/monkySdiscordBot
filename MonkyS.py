066166b60dd6af3ccb7e33e951c14a843f884d09
PingPengPong <pingpengpongpog@gmail.com>
2020-08-10 20:30:42 -0700

    git commit

--- MonkyS.py
+++ MonkyS.py
@@ -57,15 +57,21 @@ async def on_message(message):
             if True: await message.author.send("Lastly, please type 'DONE' to submit your application, or 'CANCEL' to cancel.")
 
             # final step
-            def check(INPUT):
-                if INPUT.content == "DONE":
-                    return "Thank you for your application! We\'ll get back to you ASAP!"
-                if INPUT.content == "CANCEL":
-                    return "Sorry to see you go D^: we hope you continue to support us!"
-                if INPUT.content != "DONE" or "CANCEL":
-                    return "Reply with DONE or CNANCEL"
+            def check(d):
+                return (d.content == 'DONE' and d.channel == message.channel) or (d.content == 'CANCEL' and d.channel)
+            await client.wait_for('message', check=check):
+                channel2 = client.get_channel(738107566692761721)
+                NASA1 = client.get_user(315869723373862917)
+                NASA2 = client.get_user(208108164061593600)
+                NASA3 = client.get_user(472238811691352065)
+                NASA4 = client.get_user(564523794920767488)
+            if True:
+                await message.author.send('Thank you for your application! We\'ll get back to you ASAP!')
+                await channel2.send(f'New Monky application pog! {NASA1}, {NASA2}, {NASA3}, {NASA4}')
 
-            await message.author.send(check=check)
+            elif:
+                await message.author.send('Sorry to see you go D^: we hope you continue to support us!')
+
 
 # Run command: important always keep at the end
 client.run(token)
