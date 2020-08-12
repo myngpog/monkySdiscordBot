c5fcc3ec7d07cf8dfd774ca6eb6c71cfe5e481ce
PingPengPong <pingpengpongpog@gmail.com>
2020-08-11 17:20:57 -0700

    git commit

--- MonkyS.py
+++ MonkyS.py
@@ -54,7 +54,7 @@ async def on_message(message):
             if True: await message.author.send('Link your test or past work (if test, make sure we have edit/suggestion perms)')
             # Finale
             await client.wait_for('message', check=check)
-            if True: await message.author.send("Lastly, please type 'DONE' to submit your application, or don't reply to not submit lmao.")
+            if True: await message.author.send("Lastly, please type 'DONE' to submit your application, or type 'CANCEL' to cancel lmao.")
 
             # 2nd bruh bc im dumb
             def check(s):
@@ -65,8 +65,15 @@ async def on_message(message):
             if str(bruh.content) == 'DONE':
                 await message.channel.send('Thank you for your application! We\'ll get back to you ASAP!')
 
+            channel2 = client.get_channel(738107566692761721)
+            NASA1 = client.get_user(315869723373862917)
+            NASA2 = client.get_user(208108164061593600)
+            NASA3 = client.get_user(472238811691352065)
+            NASA4 = client.get_user(564523794920767488)
+
             if str(bruh.content) == 'CANCEL':
                 await message.channel.send('Sorry to see you go D^: we hope you continue to support us!')
+                await channel2.send(f'New application pog! {NASA1}, {NASA2}, {NASA3}, {NASA4}')
 
             else:
                 pass
