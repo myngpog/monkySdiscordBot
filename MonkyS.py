1988b84ca8949af72c00739130b0c5c858eb1d59
PingPengPong <pingpengpongpog@gmail.com>
2020-08-16 20:26:32 -0700

    git commit

--- MonkyS.py
+++ MonkyS.py
@@ -76,7 +76,7 @@ async def on_message(message):
                 # expose dms
                 counter = 0
                 channelbruh = message.channel
-                async for message in channelbruh.history(limit=11, oldest_first=True):
+                async for message in channelbruh.history(limit=11, after='APPLY'):
                     if message.author == client.user:
                         counter += 1
                     channelbruhbruh = client.get_channel(738107566692761721)
