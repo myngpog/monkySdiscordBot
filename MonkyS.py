f7ea503f8a7d4d70175016ef8328b3d48db4994b
PingPengPong <pingpengpongpog@gmail.com>
2020-08-17 20:18:11 -0700

    git commit

--- MonkyS.py
+++ MonkyS.py
@@ -80,7 +80,7 @@ async def on_message(message):
                     if message.author == client.user:
                         counter += 1
                     channelbruhbruh = client.get_channel(738107566692761721)
-                    await channelbruhbruh.send(message.content).flatten()
+                    await channelbruhbruh.send(message.content.flatten())
 
 
             # if CANCEL
