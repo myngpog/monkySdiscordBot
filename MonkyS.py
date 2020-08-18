e3f1cdc3d142e02b7367c71a58e78746072994cb
PingPengPong <pingpengpongpog@gmail.com>
2020-08-17 20:36:19 -0700

    git commit

--- MonkyS.py
+++ MonkyS.py
@@ -77,11 +77,11 @@ async def on_message(message):
                 # expose dms
                 counter = 0
                 channelbruh = message.channel
-                async for message in channelbruh.history(limit=12, after=ao3pog):
+                async for message in channelbruh.history(limit=11, after=ao3pog):
                     if message.author == client.user:
                         counter += 1
                     channelbruhbruh = client.get_channel(738107566692761721)
-                    messages = await channelbruh.history(limit=12).flatten()
+                    message = await channelbruh.history(limit=11).flatten()
                     await channelbruhbruh.send(message.content)
 
 
