9e2364e8ec32dea9699983562566d6b36300a34c
PingPengPong <pingpengpongpog@gmail.com>
2020-08-28 10:56:07 -0700

    git commmit

--- MonkyS.py
+++ MonkyS.py
@@ -93,12 +93,27 @@ async def on_message(message):
             # Else
             else:
                 pass
+    # Staff welcome message
+    if message.channel == client.get_channel(736735728364683264):
+        if str(message.content.startswith) == '!welcome':
+            welcomepog = client.get_channel(736735728364683264)
+            def check(welcome):
+                return welcome.channel == client.get_channel(736735728364683264)
+            email = client.get_channel(736992278308192326)
+            links = client.get_channel(736741434652360765)
+            allocations = client.get_channel(736747201275101234)
+            updates = client.get_channel(736747148263292929)
+            await welcomepog.send(f"Welcome! To start off, please put your email under {email.mention} for future references."
+                                  f"**Fill out the staff spreadsheet** under {links.mention}, our scans drive link is also under there."
+                                  f"After you're done with a chapter, please update under {updates.mention} and *update the sheets*"
+                                  f"To see what series you're assigned, look under {allocations.mention}."
+                                  f"Once again, welcome!", check=check(welcomepog))
 
 
 # bot playing game
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game(name='monkyS is looking for new recruits aha haa'))
+    await client.change_presence(activity=discord.Game(name='we looking for ITLs, CLRDS, and KTLs!'))
 
 
 # upload image
