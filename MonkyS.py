99502940a219c8ce5e924980d8eddca14e177982
myngbruh <71903484+myngbruh@users.noreply.github.com>
2021-01-25 12:05:08 -0800

    git commit

--- MonkyS.py
+++ MonkyS.py
@@ -62,14 +62,17 @@ async def on_message(message):
             await client.wait_for('message', check=check)
             await message.author.send("Lastly, please type 'DONE' to submit your application, or type 'CANCEL' to cancel lmao **(don't bold it)**. *If i don't respond after your reply, redo the app process again by typing 'APPLY'.*")
 
-
+            # 2nd bruh bc im dumb but idk waht this is for
+            def check(s):
+                return (s.author == message.author and s.channel == message.channel)
 
             bruh = await client.wait_for('message', check=check)
 
             # if DONE
             if str(bruh.content) == 'DONE':
                 await message.channel.send('Thank you for your application! We\'ll get back to you ASAP!')
-
+                secret = client.get_channel(736704193905164441)
+                await secret.send("jfiseojfioesfiojoi")
 
 
 
