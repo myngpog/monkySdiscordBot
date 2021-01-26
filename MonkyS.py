92f6b0d424953f333051b11f6d82d82315deff69
myngbruh <71903484+myngbruh@users.noreply.github.com>
2021-01-25 19:02:23 -0800

    git commit

--- MonkyS.py
+++ MonkyS.py
@@ -84,7 +84,8 @@ async def on_message(message):
                 # expose dms
                 counter = 0
                 channelbruh = message.channel
-                async for messages in channelbruh.history(limit=10, after=ao3pog).flatten():
+                async for message in channelbruh.history(limit=10, after=ao3pog):
+                    messages = await channelbruh.history.flatten()
                     if message.author == client.user:
                         counter += 1
                     channelbruhbruh = client.get_channel(738107566692761721)
