576a0d1efa62ecf69e62deddfb6359d255da6e15
myngbruh <71903484+myngbruh@users.noreply.github.com>
2021-01-30 19:14:10 -0800

    git commit

--- MonkyS.py
+++ MonkyS.py
@@ -179,7 +179,7 @@ async def on_message(message):
             await actual_work.send(bot_help)
         if message.content.startswith("-whalecum"):
             user = message.mentions[0]
-            await ss.send(f"Welcome " + user.mention + "! To start off, please put your email under {email.mention} for future references."
+            await actual_work.send(f"Welcome " + user.mention + "! To start off, please put your email under {email.mention} for future references."
         f"** Fill out the staff spreadsheet** under {links.mention}, **our scans drive link is also under there**."
         f" After you're done with a chapter, please update under {updates.mention} and *update the sheets*."
         f" To see what stuff you're assigned, look at the sheets or get a link to the sheets through our bot under {not_bot.mention} by doing -halp."
