2ddfb363837d8a60b8713888213739e15d97b658
myngbruh <71903484+myngbruh@users.noreply.github.com>
2021-01-31 15:56:05 -0800

    git commit

--- MonkyS.py
+++ MonkyS.py
@@ -242,12 +242,12 @@ async def on_message(message):
     #bot testing tingz
     hiatus_List = []
     if message.channel == bot_test:
-        if message.content("-hiatusadd"):
+        if message.content.startswith("-hiatusadd"):
             msg = client.wait_for('message')
             if msg:
                 hiatus_List.append(str(msg))
                 await bot_test("Added to hiatus list! When you are off hiatus, please do -hiatusremove [the name you inputted]")
-        if message.content("-hiatus"):
+        if str(message.content) == "-hiatus":
             people = "\n".join(map(str, hiatus_List))
             await bot_test.send("**People on hiatus:**\n" + people)
 
