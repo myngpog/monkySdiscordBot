158de19e7e8d906a290a21b03571b9b2f57b3abf
myngbruh <71903484+myngbruh@users.noreply.github.com>
2021-01-31 19:14:43 -0800

    git commit

--- MonkyS.py
+++ MonkyS.py
@@ -245,18 +245,16 @@ hiatus_List = []
 test = client.get_channel(738107566692761721)
 @client.command()
 async def hiatusadd(ctx, arg):
-    if ctx.channel == test:
-        messages = await ctx.send(f'{arg} has been added to the hiatus list! To remove please do -hiatusremove {arg}')
-        for ctx.message in messages:
-            hiatus_List.append(arg)
+    messages = await ctx.send(f'{arg} has been added to the hiatus list! To remove please do -hiatusremove {arg}')
+    for ctx.message in messages:
+        hiatus_List.append(arg)
 
 
 hiatus_Final = (f'**Staff on hiatus/quit:**' + '\n'.join([''.join(map(str, item)) for item in hiatus_List]))
 
 @client.command()
 async def hiatus(ctx):
-    if ctx.channel == test:
-        await ctx.send(hiatus_Final)
+    await ctx.send(hiatus_Final)
 
 
 
