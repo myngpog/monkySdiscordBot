0e18723e2cbc8b701ff79435ad5d2b3188cdfd92
myngbruh <71903484+myngbruh@users.noreply.github.com>
2021-02-08 15:39:08 -0800

    git commit

--- MonkyS.py
+++ MonkyS.py
@@ -250,7 +250,7 @@ async def hiatusadd(ctx, arg):
                     hiatusbrr = json.load(f)
             hiatusbrr["Hiatus"].append(arg)
             with open("hiatus.txt", "a") as f:
-                json.dump(hiatusbrr, f)
+                f.write(json.dump(hiatusbrr, f))
                 f.seek(0)
                 f.close()
             await ctx.message("peng wuz here")
