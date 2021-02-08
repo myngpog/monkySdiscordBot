67972adca69b49c85de7b62640009c9446ce1629
myngbruh <71903484+myngbruh@users.noreply.github.com>
2021-02-08 13:58:17 -0800

    git commit

--- MonkyS.py
+++ MonkyS.py
@@ -248,13 +248,12 @@ async def hiatusadd(ctx, arg):
         messages = await ctx.channel.history(limit=1, before=senpog).flatten()
         for ctx.message in messages:
             counter += 1
-            json_file = open("hiatus.txt", "a")
+            json_file = open("hiatus.txt", "a+")
             hiatus1 = json.load(json_file)
             hiatus1["Hiatus"].append(arg)
             json.dump(hiatus1, json_file)
             json_file.seek(0)
             json_file.close()
-            await ctx.send('Peng wuz here')
 
 
 
