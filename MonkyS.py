d75450c0fae2ad1e182d03e0189b947d9dc116ba
myngbruh <71903484+myngbruh@users.noreply.github.com>
2021-06-10 18:48:28 -0700

    git push heroku main

--- MonkyS.py
+++ MonkyS.py
@@ -154,10 +154,10 @@ async def on_message(message):
         if message.content.startswith("-whalecum"):
             user = message.mentions[0]
             await actual_work.send(f"Welcome " + user.mention + f"! \n> To start off, please put your email under {email.mention} for future references."
-        f"\n**> Fill out the staff spreadsheet** under {links.mention}, **our scans drive link is also under there**."
-        f"\n> After you're done with a chapter, please update under {updates.mention} and *update the sheets*."
-        f"\n> To see what stuff you're assigned, look at the sheets or get a link to the sheets through our bot under {not_bot.mention} by doing `-halp`."
-        f"\n> Feel free to introduce yourself under {staffintro.mention}."
+        f"\n** Fill out the staff spreadsheet** under {links.mention}, **our scans drive link is also under there**."
+        f"\n After you're done with a chapter, please update under {updates.mention} and *update the sheets*."
+        f"\n To see what stuff you're assigned, look at the sheets or get a link to the sheets through our bot under {not_bot.mention} by doing `-halp`."
+        f"\n Feel free to introduce yourself under {staffintro.mention}."
         f" Once again, welcome! We're glad you're here :D")
         if str(message.content) == '-guide':
             await actual_work.send(guide)
